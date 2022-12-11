b00747093398d453647559ffce06bc132b35e7a8
mehedihasan03 <mdmehedi03@gmail.com>
2022-12-11 18:23:33 +0600

    productCrud

---
+++ src/main/java/com/example/ReactiveMongoDbCrudPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoDbCrudPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMongoDbCrudPracticeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/service/ProductService.java
@@ -0,0 +1,53 @@
+package com.example.service;
+
+import com.example.dto.ProductDto;
+import com.example.entity.Product;
+import com.example.repository.ProductRepository;
+import com.example.utils.AppUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Range;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public Flux<ProductDto> getProducts() {
+        return repository
+                .findAll()
+                .map(AppUtils::entityToDto);
+    }
+
+    public Mono<ProductDto> getProduct(String id) {
+        return repository.findById(id).map(AppUtils::entityToDto);
+    }
+
+    public Flux<ProductDto> getProductInPriceRange(double min, double max) {
+        return repository.findByPriceBetween(Range.closed(min, max));
+    }
+
+    public Mono<ProductDto> saveProduct(Mono<ProductDto> productDtoMono) {
+        return productDtoMono.map(AppUtils::dtoToEntity)
+                .flatMap(repository::insert)
+                .map(AppUtils::entityToDto);
+
+    }
+
+    public Mono<ProductDto> updateProduct(Mono<ProductDto> productDtoMono, String id) {
+        return repository.findById(id)
+                .flatMap(p -> productDtoMono
+                        .map(AppUtils::dtoToEntity).doOnNext(e -> e.setId(id))
+                )
+                .flatMap(repository::save)
+                .map(AppUtils::entityToDto);
+    }
+
+    public Mono<Void> deleteProduct(String id){
+        return repository.deleteById(id);
+    }
+}
